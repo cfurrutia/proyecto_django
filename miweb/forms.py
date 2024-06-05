2d03faa958c5ab87aecf72459b07bc50ced8a5fb
Carlos Urrutia <ing.cfurrutia@gmail.com>
2024-06-05 19:16:47 -0400

    respaldo

--- miweb/forms.py
+++ miweb/forms.py
@@ -1,13 +1,38 @@
 from django import forms
 from .models import ContactForm
-        
-class ContactFormForm(forms.Form):
-    customer_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Tu nombre'}))
-    customer_email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Tu correo electrónico'}))
-    message = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Tu mensaje'}))
-
 
 class ContactFormForm(forms.ModelForm):
     class Meta:
         model = ContactForm
         fields = ['customer_name', 'customer_email', 'message']
+        labels = {
+            'customer_name': 'Nombre',
+            'customer_email': 'Email',
+            'message': 'Mensaje',
+        }
+        widgets = {
+            'customer_name': forms.TextInput(attrs={
+                'id': 'form4Example1', 
+                'class': 'form-control', 
+                'data-mdb-input-init': True,
+                'placeholder': ' '
+            }),
+            'customer_email': forms.EmailInput(attrs={
+                'id': 'form4Example2', 
+                'class': 'form-control', 
+                'data-mdb-input-init': True,
+                'placeholder': ' '
+            }),
+            'message': forms.Textarea(attrs={
+                'id': 'form4Example3', 
+                'class': 'form-control', 
+                'data-mdb-input-init': True, 
+                'rows': 4,
+                'placeholder': ' '
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ContactFormForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] += ' form-outline mb-4'
---
+++ miweb/static/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    import { Collapse, Ripple, initMDB } from "mdb-ui-kit";
+
+    initMDB({ Collapse, Ripple });
+
+});
+
+
+
